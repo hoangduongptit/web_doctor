8f7847891937e7f066318abd9cf84c8682ef4957
hoangduongptit <duongph@ivy.com.vn>
2024-06-06 14:58:11 +0700

    update 2

--- src/components/Home/HeaderTop/HeaderTop.js
+++ src/components/Home/HeaderTop/HeaderTop.js
@@ -5,7 +5,7 @@ const HeaderTop = () => {
     return (
         <div style={{ height: "600px", 'width': '100%' }} className="row d-flex align-items-center container">
             <div className="col-md-4 col-sm-6 col-12 offset-md-1 md-mx-5">
-                <h1>Your New Smile <br />Starts From Here</h1>
+                <h1>11111Your New Smile <br />Starts From Here</h1>
                 <p className="text-secondary">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore eveniet necessitatibus et iusto corrupti minima
                 </p>
                 <Link to="/appointment" className="btn btn-primary btn-lg shadow rounded"> GET STARTED</Link>
